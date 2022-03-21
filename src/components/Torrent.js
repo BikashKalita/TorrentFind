082d43e1bfbe54ebdb34072ad26dfa2e528c259d
BikashKalita <56755874+BikashKalita@users.noreply.github.com>
2022-03-21 15:10:11 +0530

    🔎 YTS Copy Issue Fixed

--- src/components/Torrent.js
+++ src/components/Torrent.js
@@ -6,50 +6,21 @@ import {
   CardActions,
   Link,
 } from "@mui/material";
+import YtsCard from "./TCard";
 const Torrent = ({ t, s }) => {
   const [isCopied, setCopied] = useState(false);
   const copyHandler = async (text) => {
     if ("clipboard" in navigator) {
       await navigator.clipboard.writeText(text);
       setCopied(true);
-    } else {
-      document.execCommand("copy", true, text);
-      setCopied(true);
     }
   };
   return (
     <>
       {s === "yts" ? (
-        t.quality.map((q) => (
-          <Card sx={{ mb: 1 }}>
-            <CardContent>
-              <Typography variant="h6" gutterBottom>
-                {t.name} ({q.quality})
-              </Typography>
-              <Typography variant="body2">
-                Size: {q.size} | Leech: {q.leech} | Seeds: {q.seed}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Link
-                src={q.magnet}
-                underline="none"
-                sx={{ cursor: "pointer" }}
-                component="button"
-              >
-                <Typography
-                  variant="button"
-                  onClick={() => copyHandler(q.magnet)}
-                >
-                  {/* Todo (Fixed Copied) */}
-                  {isCopied ? "Magnet" : "Magnet"}
-                </Typography>
-              </Link>
-            </CardActions>
-          </Card>
-        ))
+        t.quality.map((q) => <YtsCard t={t} q={q} key={q.magnet} />)
       ) : (
-        <Card sx={{ mb: 1 }}>
+        <Card sx={{ mb: 1 }} key={t.magnet}>
           <CardContent>
             <Typography variant="h6" gutterBottom>
               {t.name}
